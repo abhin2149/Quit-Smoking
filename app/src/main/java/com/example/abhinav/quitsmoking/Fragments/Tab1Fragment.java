5a70ea75555cb1827d323cfb610c7a410625dc62
ashutosh160798 <35541355+ashutosh160798@users.noreply.github.com>
2018-10-26 23:52:54 +0530

    Layout created

---
+++ app/src/main/java/com/example/abhinav/quitsmoking/Fragments/Tab1Fragment.java
@@ -0,0 +1,30 @@
+package com.example.abhinav.quitsmoking.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.abhinav.quitsmoking.R;
+
+/**
+ * Created by ashu on 26-10-2018.
+ */
+
+public class Tab1Fragment extends Fragment {
+    public Tab1Fragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/abhinav/quitsmoking/Fragments/Tab3Fragment.java
@@ -0,0 +1,31 @@
+package com.example.abhinav.quitsmoking.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.abhinav.quitsmoking.R;
+
+/**
+ * Created by ashu on 26-10-2018.
+ */
+
+public class Tab3Fragment extends Fragment {
+
+    public Tab3Fragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_three, container, false);
+    }
+}
